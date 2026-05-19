8c33e55a9d7fc1b62006106bc13fe9899a5e62a7
dralend <starforge_drace@yahoo.com>
2026-05-18 20:05:01 -0700

    project

---
+++ intro-to-game-engines/scripts/player.gd
@@ -0,0 +1,50 @@
+extends CharacterBody2D
+
+
+const SPEED = 120.0
+const JUMP_VELOCITY = -300.0
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+var double_jump: bool = false
+
+func _physics_process(delta: float) -> void:
+	if is_on_floor() and double_jump == true:
+		double_jump = false
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			jump()
+		elif double_jump == false:
+			double_jump = true
+			jump()
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+	
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else :
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+	
+func jump(multi : float = 1):
+		velocity.y = JUMP_VELOCITY * multi
